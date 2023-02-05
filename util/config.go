fa853ee8e39ad7011029e654e3e28c883bbb6963
asadzeynal <asadzeynal9@gmail.com>
2023-02-05 04:54:05 +0400

    Implemented ability to set config

--- util/config.go
+++ util/config.go
@@ -1,18 +1,21 @@
 package util
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	RedditAuth string `mapstructure:"REDDIT_AUTH"`
-	TgToken    string `mapstructure:"TG_TOKEN"`
+	RedditAuth        string `mapstructure:"reddit-auth"`
+	TgToken           string `mapstructure:"tg-token"`
+	RedditAccessToken string `mapstructure:"reddit-access-token"`
+	TokenRefreshAt    string `mapstructure:"token-refresh-at"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
-	viper.SetConfigType("env")
-
-	viper.AutomaticEnv()
+	viper.SetConfigType("yaml")
 
 	err = viper.ReadInConfig()
 	if err != nil {
@@ -21,4 +24,14 @@ func LoadConfig(path string) (config Config, err error) {
 
 	err = viper.Unmarshal(&config)
 	return
+}
+
+func (c *Config) Set(key string, value string) error {
+	viper.Set(key, value)
+	err := viper.WriteConfig()
+	if err != nil {
+		return fmt.Errorf("unable to write config: %v", err)
+	}
+	err = viper.Unmarshal(&c)
+	return err
 }
