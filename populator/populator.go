a233eaa6fe062e320065bcfa3bb1d5cac3b5c783
asadzeynal <asadzeynal9@gmail.com>
2023-02-20 22:33:04 +0400

    Added db populator that will be scheduled to run

---
+++ populator/populator.go
@@ -0,0 +1,53 @@
+package populator
+
+import (
+	"context"
+	"fmt"
+
+	db "github.com/asadzeynal/TgRedditHotBot/db/sqlc"
+	"github.com/asadzeynal/TgRedditHotBot/rdClient"
+)
+
+func Start(store *db.SQLStore, client rdClient.Client) error {
+	posts, err := client.FetchPosts()
+	if err != nil {
+		return fmt.Errorf("error while retrieving posts: %v ", err)
+	}
+
+	for i := range posts {
+		store.ExecTx(context.Background(), func(queries *db.Queries) error {
+			post := posts[i]
+			_, err = queries.CreatePost(context.Background(), db.CreatePostParams{ID: post.Id, Title: post.Title, Url: post.Url})
+			if err != nil {
+				return fmt.Errorf("unable to store post: %v", err)
+			}
+
+			if post.ContentType == "image" {
+				_, err := store.CreatePostImage(context.Background(), db.CreatePostImageParams{
+					Post: post.Id,
+					Url:  post.ImageUrl,
+				})
+				if err != nil {
+					return fmt.Errorf("unable to store image: %v", err)
+				}
+
+			} else if post.ContentType == "video" {
+				_, err := store.CreatePostVideo(context.Background(), db.CreatePostVideoParams{
+					Post:     post.Id,
+					Height:   int32(post.Video.Height),
+					Width:    int32(post.Video.Width),
+					Duration: int32(post.Video.Duration),
+					Url:      post.Video.Url,
+				})
+				if err != nil {
+					return fmt.Errorf("unable to store image: %v", err)
+				}
+			}
+
+			return nil
+		})
+	}
+
+	return nil
+
+}
