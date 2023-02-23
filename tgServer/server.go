aaf79b5b6fbb1127228c3480b5edc38557db3741
asadzeynal <asadzeynal9@gmail.com>
2023-02-23 18:29:04 +0400

    Minor fix for the server

--- tgServer/server.go
+++ tgServer/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	db "github.com/asadzeynal/TgRedditHotBot/db/sqlc"
 	"github.com/asadzeynal/TgRedditHotBot/rdClient"
@@ -26,12 +27,9 @@ type Server struct {
 func Start(config util.Config, client *rdClient.Client, store db.Store) error {
 	server := Server{client, store}
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "ok") })
-	http.ListenAndServe(":8090", nil)
-
 	pref := telebot.Settings{
-		Token: config.TgToken,
-		// Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		Token:  config.TgToken,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 	}
 	bot, err := telebot.NewBot(pref)
 	if err != nil {
@@ -39,13 +37,14 @@ func Start(config util.Config, client *rdClient.Client, store db.Store) error {
 	}
 
 	menu.Reply(menu.Row(btnMorePosts))
-
 	bot.Handle("/start", server.start)
 	bot.Handle(&btnMorePosts, server.getRandomPost)
-	bot.SetWebhook(&telebot.Webhook{Listen: ":8080"})
 
 	log.Println("starting reddit tg server")
 	bot.Start()
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "ok") })
+	http.ListenAndServe(":8090", nil)
 
 	return nil
 }
