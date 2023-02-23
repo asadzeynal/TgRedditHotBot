55428053a9f819a34cb5ecb089783c2aa3351590
asadzeynal <asadzeynal9@gmail.com>
2023-02-23 15:53:09 +0400

    Added simple health check

--- tgServer/server.go
+++ tgServer/server.go
@@ -3,6 +3,7 @@ package tgServer
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	db "github.com/asadzeynal/TgRedditHotBot/db/sqlc"
@@ -25,6 +26,11 @@ type Server struct {
 
 func Start(config util.Config, client *rdClient.Client, store db.Store) error {
 	server := Server{client, store}
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "health: ok\n")
+	})
+	http.ListenAndServe(":8080", nil)
 
 	pref := telebot.Settings{
 		Token:  config.TgToken,
